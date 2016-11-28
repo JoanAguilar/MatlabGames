6f9b26e86317196cda27cbe0fe439e64362da93f
JoanAguilar <joana1@uci.edu>
2016-11-27 17:56:44 -0800

    Added files via upload
    Added Bangbang.m and Lander.m.

---
+++ Bangbang.m
@@ -0,0 +1,353 @@
+%
+% MATLAB BANG! BANG!
+%
+% 2-player aim game. Inspired by the 1990 Windows game "Bang! Bang!".
+% See: http://playdosgamesonline.com/bang-bang.html
+% This verision was developed for teaching purposes, as an example of game
+% developed using Matlab that does not require an extensive programming 
+% knowledge. 
+%
+% This implementation uses:
+% - Selective execution (if/switch-case statements).
+% - Recursive execution (for/while loops).
+% - Basic input/output (input/fprintf).
+% - Functions.
+% - Anonymous functions.
+% - Random number generators.
+% - Plotting tools (lines, patches, and markers).
+%
+% It does NOT use:
+% - Struct variables.
+% - Cell variables.
+% - Objects
+% - GUI commands.
+%
+% Developed by: Joan Aguilar Mayans
+% E-mail:       joana1@uci.edu
+% Date:         16-Nov-2016
+
+%
+% Main script -------------------------------------------------------------
+%
+
+function BANGBANG
+
+% Clear
+clc;
+close all;
+
+% Intro
+disp('%%%%%%%%%%%%%%%%%%%%%%');
+disp('% MATLAB BANG! BANG! %');
+disp('%%%%%%%%%%%%%%%%%%%%%%');
+fprintf('\n');
+
+% Ask the user for a terrain
+fprintf('Select scenery. Options are:\n');
+fprintf(' (1) Flat terrain\n');
+fprintf(' (2) Tilted terrain\n');
+fprintf(' (3) A hill in the middle\n');
+fprintf(' (4) A tall hill in the middle\n');
+fprintf(' (5) Player A on top of a hill\n');
+fprintf(' (6) Player B on top of a hill\n');
+fprintf(' (7) Random choice\n');
+fprintf('\n');
+ground_sel = input('Your choice: ');
+fprintf('\n');
+if ground_sel == 7
+    ground_sel = randi(6);
+end
+
+% Assign ground level as an anonymous function
+rand1 = rand();
+rand2 = rand();
+switch ground_sel
+    case 1
+        ground = @(x) zeros(size(x));  % Flat ground
+    case 2
+        ground = @(x) (0.4*rand1 - 0.2)*x;  % Tilted terrain
+    case 3
+        ground = @(x) (5*rand1 + 10)*cos(2*pi*(x - (40*rand2 + 30))/100)  + (0.4*rand2 - 0.2)*x;  % A hill in the middle
+    case 4
+        ground = @(x) (10*rand1 + 20)*cos(2*pi*(x - (40*rand2 + 30))/100)  + (0.4*rand2 - 0.2)*x;  % A tall hill in the middle
+    case 5
+        ground = @(x) (30*rand1 + 20)./(1 + exp(0.2*(x - 50)));  % A on top of a hill
+    case 6
+        ground = @(x) (30*rand1 + 20)./(1 + exp(-0.2*(x - 50)));  % B on top of a hill
+    otherwise
+        ground = @(x) zeros(size(x));  % Flat ground
+end
+
+% Assign players position
+pos_A = [0; ground(0)];
+pos_B = [100; ground(100)];
+
+% Ask the user for wind
+fprintf('Select wind. Options are:\n');
+fprintf(' (1) No wind\n');
+fprintf(' (2) Gentle breeze\n');
+fprintf(' (3) Strong breeze\n');
+fprintf(' (4) Strong gusty breeze\n');
+fprintf(' (5) Strong gale\n');
+fprintf(' (6) Strong gusty gale\n');
+fprintf(' (7) Hurricane mode\n');
+fprintf(' (8) Random choice\n');
+fprintf('\n');
+wind_sel = input('Your choice: ');
+fprintf('\n');
+if wind_sel == 8
+    wind_sel = randi(7);
+end
+
+% Assign wind level as an anonymous function
+rand3 = rand();
+rand4 = rand();
+switch wind_sel
+    case 1  % No wind
+        nominal_wind = 0;
+        gusts = 0;
+    case 2  % Gentle breeze
+        nominal_wind = sign(rand3 - 0.5)*(1.7*rand4 + 3.6);
+        gusts = 0;
+    case 3  % Strong breeze
+        nominal_wind = sign(rand3 - 0.5)*(2.5*rand4 + 11.4);
+        gusts = 0;
+    case 4  % Strong gusty breeze
+        nominal_wind = sign(rand3 - 0.5)*(2.5*rand4 + 11.4);
+        gusts = 1;
+    case 5  % Strong gale
+        nominal_wind = sign(rand3 - 0.5)*(3.1*rand4 + 21.1);
+        gusts = 0;
+    case 6  % Strong gusty gale
+        nominal_wind = sign(rand3 - 0.5)*(3.1*rand4 + 21.1);
+        gusts = 1;
+    case 7  % Hurricane
+        nominal_wind = sign(rand3 - 0.5)*(3.9*rand4 + 33.1);
+        gusts = 1;
+    otherwise
+        nominal_wind = 0;
+        gusts = 0;
+end
+if ~gusts
+    wind = @(t, rand_val) [nominal_wind; 0];
+else
+    wind = @(t, rand_val) [1.5*nominal_wind + 0.5*nominal_wind*sin(2*pi*(t/20 + rand_val)); 0];
+end
+
+% Tell the user the wind situation
+fprintf('Wind report:\n');
+if nominal_wind == 0
+    fprintf('There is no wind.\n');
+else
+    if nominal_wind > 0
+        direction = 'right';
+    else
+        direction = 'left';
+    end
+    if ~gusts
+        fprintf('Constant wind of %.2f m/s towards the %s.\n', nominal_wind, direction);
+    else
+        fprintf('Gusty wind of %.2f m/s towards the %s.\n', nominal_wind, direction);
+    end
+end
+fprintf('\n');
+
+% Initiliaze turn to player A and winner flag to false
+winner_flag = 0;
+turn = 'A';
+
+% Plot scenery
+fig = plotscenery(pos_A, pos_B, ground);
+
+% Keep playing turns until there is a winner
+while ~winner_flag
+    
+    % Display turn information and ask the player for angle and velocity
+    fprintf('It''s %c''s turn.\n', turn);
+    angle = input('Select angle (degrees): ');  % In degrees
+    vel = input('Select velocity (m/s):  ');  % In m/s
+    
+    % Simulate each shot
+    switch turn
+        case 'A'
+            [winner_flag, trajectory, dt] = fire(pos_A, angle, vel, pos_B, ground, wind);
+        case 'B'
+            angle = 180 - angle;
+            [winner_flag, trajectory, dt] = fire(pos_B, angle, vel, pos_A, ground, wind);
+    end
+    
+    % Plot the shot
+    plottrajectory(fig, trajectory, dt, turn);
+    
+    % If there isn't a winner, tell the player that he/she missed and
+    % change the turn variable
+    if ~winner_flag
+        fprintf('You missed!\n\n');
+        switch turn
+            case 'A'
+                turn = 'B';
+            case 'B'
+                turn = 'A';
+        end
+    end
+end
+
+% After there is a winner, display it on screen.
+fprintf('Player %c wins!\n', turn);
+
+end
+
+%
+% Subfunctions ------------------------------------------------------------
+%
+
+function fig = plotscenery(pos_A, pos_B, ground)
+% PLOTSCENERY generates a figure with the basic landscape for the game
+% Bang! Bang! It places player A at pos_A, player B at pos_B and draws the
+% ground using function ground.
+% fig: figure handle where the scenery is plotted.
+% pos_A: vector containing the x and y coordinates for player A.
+% pos_B: vector containing the x and y coordinates for player B.
+% ground: a function handle that defines the ground level (if y >
+% ground(x), y is above the ground at coordinate x.
+%
+% Created by: Joan Aguilar Mayans
+% E-mail:     joana1@uci.edu
+% Date:       17-Nov-216
+
+% Create figure
+fig = figure;
+hold on;
+
+% Generate axis
+delta_hor = abs(pos_B(1) - pos_A(1));  % Horizontal distance between players
+delta_ver = abs(pos_B(2) - pos_A(2));  % Vertical distance between players
+max_delta = max(delta_hor, delta_ver);
+mid = (pos_A + pos_B)/2;  % Middle point between the two players.
+min_x_axis = mid(1) - max_delta/2 - 10;
+max_x_axis = mid(1) + max_delta/2 + 10;
+min_y_axis = mid(2) - max_delta/2 - 10;
+max_y_axis = mid(2) + max_delta/2 + 10;
+axis([min_x_axis, max_x_axis, min_y_axis, max_y_axis]);
+axis square;
+
+% Generate horizon points
+horiz_x = linspace(min_x_axis, max_x_axis, 100);
+horiz_y = ground(horiz_x);
+
+% Plot ground and sky
+patch([horiz_x, max_x_axis, min_x_axis], [horiz_y, min_y_axis, min_y_axis], [0, 0.6, 0]);
+patch([horiz_x, max_x_axis, min_x_axis], [horiz_y, max_y_axis, max_y_axis], [0, 1, 1]);
+
+% Plot players as markers
+plot(pos_A(1), pos_A(2), 'Marker','s', 'MarkerFaceColor', 'red', 'MarkerSize', 20);
+text(pos_A(1) - 5, pos_A(2) + 5, 'A');
+plot(pos_B(1), pos_B(2), 'Marker','s', 'MarkerFaceColor', 'blue', 'MarkerSize', 20);
+text(pos_B(1) + 5, pos_B(2) + 5, 'B');
+
+end
+
+function [hit, traj, dt] = fire(your_pos, angle, lin_vel, enemy_pos, ground, wind)
+% FIRE computes the trajectory of a shot and checks if it hits the enemy.
+% hit: either true or false depending if the shot ends up close enough to 
+% enemy_pos.
+% traj: an array containing points equally spaced in time from the shot
+% your_pos: a column vector containing the position of the player that is 
+% shooting.
+% dt: time step used to compute the trajectory.
+% angle: angle of the shot.
+% vel: velocity of the shot.
+% enemy_pos: a column vector containing the position of the player that is 
+% being shot.
+% ground: a function that defines the ground level (m) as a function of x 
+% (if y > ground(x), y is above the ground at coordinate x).
+% wind: a function that defines the wind speed (m/s) as a function of time 
+% and a random parameter between 0 and 1
+%
+% Created by: Joan Aguilar Mayans
+% E-mail:     joana1@uci.edu
+% Date:       17-Nov-216
+
+% Note: variable allocation can be made more efficient.
+
+% Define simulation parameters
+m = 10;  % mass in kg;
+r = 0.1;  % radius in m
+rho_air = 1.225;  % air density at sea level in kg/m3
+cd = 0.47;  % Sphere drag coefficient
+A = pi*r^2;  % Reference area (m2)
+g = [0; -9.81];  % Gravity vector in m/s2
+
+% Define integration time step
+dt = 0.01;
+
+% Define wind random parameter
+rand_par = rand();
+
+% Initialize
+t = 0;
+traj = your_pos;
+vel_vec = [lin_vel*cosd(angle);
+           lin_vel*sind(angle)];
+
+% Compute trajectory
+while (traj(2,end) >= ground(traj(1,end)))
+    
+    % Compute current acceleration, velocity, and position
+    u = wind(t, rand_par) - vel_vec(:,end);  % Flow velocity
+    F_d = 0.5*rho_air*cd*A*norm(u)*u;  % Aerodynamic drag force
+    acc = (m*g + F_d)/m;  % Compute acceleration
+    vel = vel_vec(:,end);  % Compute velocity
+    pos = traj(:,end); % Compute position
+    
+    % Compute next step velocity and position (semi-implicit Euler)
+    vel_plus = vel + acc*dt;
+    pos_plus = pos + vel_plus *dt;
+    
+    % Update time
+    t = t + dt;
+    
+    % Store
+    vel_vec = [vel_vec, vel_plus];
+    traj = [traj, pos_plus];
+end
+
+% Check if there is a hit
+hit = norm(traj(:,end) - enemy_pos) < 1;
+end
+
+function plottrajectory(fig, traj, dt, turn)
+% PLOTTRAJECTORY plots the trajectory defined by traj on the scenery. The
+% color depennds on the variable turn.
+% fig: figure handle where the scenery is plotted.
+% traj: an array containing the points of the trajectory to plot.
+% dt: time step used to compute the trajectory.
+% turn: is either A or B and determines the color in which the trajectory
+% is plotted (A is red and B is blue).
+%
+% Created by: Joan Aguilar Mayans
+% E-mail:     joana1@uci.edu
+% Date:       17-Nov-216
+
+% Get number of points in the trajectory
+N = size(traj, 2);
+
+% Get color
+if turn == 'A'
+    color = [1, 0, 0];
+elseif turn == 'B'
+    color = [0, 0, 1];
+end
+
+% Plot
+figure(fig);
+for n = 2:N
+    tic;
+    plot([traj(1, n - 1), traj(1, n)], [traj(2, n - 1), traj(2, n)], '-', 'Color', color);
+    pause(dt - toc);
+end
+
+% Emphasize last point
+plot(traj(1, end), traj(2, end), 'Marker', 'o', 'MarkerFaceColor', 'black', 'MarkerSize', 20);
+
+end
---
+++ Lander.m
@@ -0,0 +1,547 @@
+%
+% MATLAB MOON LANDER
+%
+% 2D Moon landing simulation game. Inspired by the 1990 Windows game 
+% "Lander". See for instance:
+% https://play.google.com/store/apps/details?id=com.pilot51.lander
+% The player needs to land the lunar module safely on the Moon using a
+% limitted amount of fuel. This game was developed as an example of a 
+% simple Matlab game and to test different Matlab capabilities.
+%
+% Developed by: Joan Aguilar Mayans
+% E-mail:       joana1@uci.edu
+% Date:         21-Nov-2016
+
+%
+% Main script -------------------------------------------------------------
+%
+
+function Lander2
+
+% Clear
+clear;
+clc;
+close all;
+
+% Intro
+disp('%%%%%%%%%%%%%%%%%');
+disp('% MATLAB LANDER %');
+disp('%%%%%%%%%%%%%%%%%');
+fprintf('\n');
+fprintf('Your mission is to land the Apollo Lunar lander safely on the moon.\n');
+fprintf('\n');
+
+% Ask the user for control options
+fprintf('Select controls. Options are:\n');
+fprintf(' (1) Arrow keys:\n');
+fprintf('      - Left arrow: RCS thrusters counter-clockwise.\n');
+fprintf('      - Up arrow: Main thruster.\n');
+fprintf('      - Right arrow: RCS thrusters clockwise.\n');
+fprintf(' (2) Numpad:\n');
+fprintf('      - 4: RCS thrusters counter-clockwise.\n');
+fprintf('      - 7: RCS thrusters counter-clockwise + main thruster.\n');
+fprintf('      - 8: Main thruster.\n');
+fprintf('      - 9: RCS thrusters clockwise + main thruster.\n');
+fprintf('      - 6: RCS thrusters clockwise.\n');
+fprintf(' (3) AWSD:\n');
+fprintf('      - A: RCS thrusters counter-clockwise.\n');
+fprintf('      - Q: RCS thrusters counter-clockwise + main thruster.\n');
+fprintf('      - W: Main thruster.\n');
+fprintf('      - E: RCS thrusters clockwise + main thruster.\n');
+fprintf('      - D: RCS thrusters clockwise.\n');
+fprintf('\n');
+controls_sel = input('Your choice: ');
+fprintf('\n');
+
+% Ask the user for a difficulty
+fprintf('Select difficulty. Options are:\n');
+fprintf(' (1) Easy:\n');
+fprintf('      - 60 seconds left of fuel.\n');
+fprintf('      - 10 m/s of max vertical velocity.\n');
+fprintf('      - 5 m/s of max horizontal velocity.\n');
+fprintf('      - 30 degrees of max terrain angle.\n');
+fprintf('      - 30 degrees of max lander-terrain angle.\n');
+fprintf(' (2) Medium:\n');
+fprintf('      - 40 seconds left of fuel.\n');
+fprintf('      - 5 m/s of max vertical velocity.\n');
+fprintf('      - 2 m/s of max horizontal velocity.\n');
+fprintf('      - 10 degrees of max terrain angle.\n');
+fprintf('      - 10 degrees of max lander-terrain angle.\n');
+fprintf(' (3) Hard:\n');
+fprintf('      - 20 seconds left of fuel.\n');
+fprintf('      - 2 m/s of max vertical velocity.\n');
+fprintf('      - 1 m/s of max horizontal velocity.\n');
+fprintf('      - 5 degrees of max terrain angle.\n');
+fprintf('      - 5 degrees of max lander-terrain angle.\n');
+fprintf(' (4) Impossible:\n');
+fprintf('      - 10 seconds left of fuel.\n');
+fprintf('      - 1 m/s of max vertical velocity.\n');
+fprintf('      - 0.5 m/s of max horizontal velocity.\n');
+fprintf('      - 2 degrees of max terrain angle.\n');
+fprintf('      - 2 degrees of max lander-terrain angle.\n');
+fprintf('\n');
+diff_sel = input('Your choice: ');
+fprintf('\n');
+
+% Assign difficulty level variables
+switch diff_sel
+    case 1
+        fuel_time = 60;
+        max_vel_y = 10;
+        max_vel_x = 5;
+        max_terrain_angle = 30*pi/180;
+        max_lander_angle = 30*pi/180;
+    case 2
+        fuel_time = 40;
+        max_vel_y = 5;
+        max_vel_x = 2;
+        max_terrain_angle = 10*pi/180;
+        max_lander_angle = 10*pi/180;
+    case 3
+        fuel_time = 20;
+        max_vel_y = 2;
+        max_vel_x = 1;
+        max_terrain_angle = 5*pi/180;
+        max_lander_angle = 5*pi/180;
+    case 4
+        fuel_time = 10;
+        max_vel_y = 1;
+        max_vel_x = 0.5;
+        max_terrain_angle = 2*pi/180;
+        max_lander_angle = 2*pi/180;
+    otherwise
+        fuel_time = 60;
+        max_vel_y = 10;
+        max_vel_x = 5;
+        max_terrain_angle = 30*pi/180;
+        max_lander_angle = 30*pi/180;
+end
+
+% Landing site dimensions
+land_dimensions = [-50 50 0 100];
+
+% Generate stars positions
+N_stars = 200;
+stars = [(land_dimensions(2) - land_dimensions(1))*rand(1,N_stars) + land_dimensions(1);
+         (land_dimensions(4) - land_dimensions(3))*rand(1,N_stars) + land_dimensions(3)];
+
+% Definie lander initial state
+pos = [30; 100];  % Position (m)
+R = diag([1 1]); % Rotation matrix
+vel = [0; 0];    % Velocity (m/s)
+w = 0;           % Angular velocity (rad/s)
+T_B = [0; 0];    % Thrust in body frame (N)
+M_G = 0;         % Applied torque (at the center of mass, Nm)
+m_dry = 4284;    % Dry mass (kg)
+m_fuel = 0.5*10463;  % Fuel mass (kg)
+m_RCS = 0.75*287;     % RCS fuel mass (kg)
+mdot_fuel = -m_fuel/fuel_time;  % Fuel flow (kg/s)
+mdot_RCS = -m_RCS/(2*fuel_time);  % RCS fuel flow (kg/s)
+contact_point_B = [0; -4.2];  % Contact point with the ground in body coordinates (m)
+
+% Define lunar gravity
+g = [0; -1.62];
+
+% Define figure
+fig = figure;
+hold on;
+
+% Plot scene
+plotscene(fig, land_dimensions, stars, @ground, pos, R, T_B, M_G);
+
+% Display information on the command window
+dispinfo(@ground, pos, R, vel, w, contact_point_B, m_dry, m_fuel, m_RCS);
+
+% Initialize values for simulation
+% key = 'downarrow';
+landed = 0;  % 0 if the lander is flying
+             % 1 if it has landed 
+             % 2 if it has crashed
+             % 3 if the user quits
+dt = 0.01;  % Time step
+while landed == 0
+    
+    % Set counter
+    tic;
+    
+    % Get the key that is being pressed
+    set(fig, 'KeyPressFcn', @keypresscallback, 'KeyReleaseFcn', @keyreleasecallback);
+    key = getappdata(0, 'key');
+    if isempty(key)
+        key = 'downarrow';
+    end
+    
+    % Exit if the user hit escape
+    if strcmp(key, 'escape')
+        landed = 3;
+        break;
+    end
+        
+
+    % Compute thrust
+    if m_fuel > 0
+        switch controls_sel
+            case 2
+                switch key
+                    case {'numpad7', 'numpad8', 'numpad9'}
+                        T_B = [0; 45040];
+                    otherwise
+                        T_B = [0; 0];
+                end
+            case 3
+                switch key
+                    case {'q', 'w', 'e'}
+                        T_B = [0; 45040];
+                    otherwise
+                        T_B = [0; 0];
+                end     
+            otherwise
+                switch key
+                    case 'uparrow'
+                        T_B = [0; 45040];
+                    otherwise
+                        T_B = [0; 0];
+                end
+        end
+    else
+        T_B = [0; 0];
+    end
+    
+    % Compute torque
+    if m_RCS > 0
+        switch controls_sel
+            case 2
+                switch key
+                    case {'numpad4', 'numpad7'}
+                        M_G = 2*440*4.29;
+                    case {'numpad9', 'numpad6'}
+                        M_G = -2*440*4.29;
+                    otherwise
+                        M_G = 0;
+                end
+            case 3
+                switch key
+                    case {'a', 'q'}
+                        M_G = 2*440*4.29;
+                    case {'e', 'd'}
+                        M_G = -2*440*4.29;
+                    otherwise
+                        M_G = 0;
+                end
+            otherwise
+                switch key
+                    case 'leftarrow'
+                        M_G = 2*440*4.29;
+                    case 'rightarrow'
+                        M_G = -2*440*4.29;
+                    otherwise
+                        M_G = 0;
+                end
+        end
+    else
+        M_G = 0;
+    end
+    
+    
+    % Integrate
+    % Recompute mass
+    switch controls_sel
+        case 2
+            switch key
+                case {'numpad4', 'numpad6'}
+                    m_RCS = m_RCS + mdot_RCS*dt;
+                case {'numpad7', 'numpad9'}
+                    m_fuel = m_fuel + mdot_fuel*dt;
+                    m_RCS = m_RCS + mdot_RCS*dt;
+                case 'numpad8'
+                    m_fuel = m_fuel + mdot_fuel*dt;
+            end
+        case 3
+            switch key
+                case {'a', 'd'}
+                    m_RCS = m_RCS + mdot_RCS*dt;
+                case {'q', 'e'}
+                    m_fuel = m_fuel + mdot_fuel*dt;
+                    m_RCS = m_RCS + mdot_RCS*dt;
+                case 'w'
+                    m_fuel = m_fuel + mdot_fuel*dt;
+            end
+        otherwise 
+            switch key
+                case 'uparrow'
+                    m_fuel = m_fuel + mdot_fuel*dt;
+                case{'leftarrow', 'rightarrow'}
+                    m_RCS = m_RCS + mdot_RCS*dt;
+            end
+    end
+    m_total = m_dry + m_fuel + m_RCS;
+    
+    % Recompute moment of inertia (as a sphere of radius = 3 m)
+    I_G = 2*m_total*3^2/5;
+    
+    % Integrate velocities
+    w = w + (1/I_G)*M_G*dt;
+    vel = vel + (g + (1/m_total)*(R*T_B))*dt;
+    
+    % Integrate position and orientation
+    th = w*dt;
+    Rstep = [cos(th), -sin(th);
+             sin(th),  cos(th)];
+    R = Rstep*R;
+    pos = pos + vel*dt;
+    
+    % Replot scene
+    % Plot scene
+    plotscene(fig, land_dimensions, stars, @ground, pos, R, T_B, M_G);
+
+    % Update information on the command window
+    dispinfo(@ground, pos, R, vel, w, contact_point_B, m_dry, m_fuel, m_RCS);
+    
+    % Check if flying, landed, or crashed
+    contact_point = R*contact_point_B + pos;
+    ground_dist = contact_point(2) - ground(contact_point(1));
+    if ground_dist > 0
+        landed = 0;
+    else
+        [~, dydx] = ground(pos(1));
+        terrain_angle = atan2(dydx, 1);
+        lander_angle = sign(R(2,2))*acos(trace(R)/2);
+        if -vel(2) < max_vel_y && abs(vel(1)) < max_vel_x && abs(terrain_angle) < max_terrain_angle && abs(lander_angle - terrain_angle) < max_lander_angle
+            landed = 1;
+        else
+            landed = 2;
+        end
+    end
+       
+    % Pause if needed
+    pause(dt - toc);
+end
+
+fprintf('\n');
+if landed == 1
+    fprintf('You landed!\n');
+elseif landed == 2
+    fprintf('You crashed!\n');
+elseif landed == 3
+    fprintf('Bye!\n');
+end
+
+end
+
+% % Flat ground
+% function [y, dydx] = ground(x)
+% % GROUND returns the ground level (y) and the gradient (dydx). y values
+% % should be at least 1.
+% 
+% y = ones(size(x));
+% dydx = zeros(size(x));
+% 
+% end
+
+% Crater
+function [y, dydx] = ground(x)
+% GROUND returns the ground level (y) and the gradient (dydx). y values
+% should be at least 1.
+
+a = 10;
+b = pi/40;
+c = 1;
+
+y = a.^(-cos(b*x)) + c;
+dydx = (b*sin(b*x).*log(a))./a.^cos(b*x);
+
+end
+
+function plotscene(fig, full_dimensions, stars, ground, pos, R, T, M)
+% PLOTSCENE plots a region of the scene of the lunar landing. The region
+% can be of 100x100 (full scene), 50x50, 20x20, or 10x10.
+% fig: a value containing the figure number.
+% dimensions: a vector containing the dimensions of the landing site using
+% format [xmin xmax ymin ymax].
+% stars: 2xN array containing the position of stars.
+% ground: a function that returns the ground level at a specific point and
+% the gradient at that point.
+% pos: a vector containing the current position of the center of mass of
+% the lander (m).
+% R: a rotation matrix containing the orientation of the lander.
+% T: a vector containing the thrust in the body frame (N).
+% M: a scalar containing the torque applied about the center of mass (Nm).
+
+% Select which region to plot
+if pos(2) > 50
+    region = full_dimensions;
+else
+    if pos(2) > 20
+        scene_side = 50;
+    elseif pos(2) > 10
+        scene_side = 20;
+    else
+        scene_side = 10;
+    end
+    region = [scene_side*floor(pos(1)/scene_side), scene_side*floor(pos(1)/scene_side) + scene_side, 0, scene_side];
+end
+
+% Figure out which starts to plot
+plotted_stars = (stars(1,:) < region(2) & stars(1,:) > region(1) & stars(2,:) < region(4) & stars(2,:) > region(3));
+
+% Compute ground level
+x_ground = linspace(region(1), region(2), 100);
+y_ground = ground(x_ground);
+
+% Select figure
+figure(fig);
+cla;
+
+% Plot sky
+patch([region(1), region(2), region(2), region(1)], [region(3), region(3), region(4), region(4)], 'black');
+
+% Plot stars
+plot(stars(1, plotted_stars), stars(2, plotted_stars), 'w.');
+
+% Plot lander
+plotlander(fig, pos, R, T, M);
+
+% Plot ground
+patch([x_ground, region(2), region(1)], [y_ground, region(3), region(3)], [.6 .6 .6]);
+
+% Set axes
+axis(region);
+axis square;
+
+% Force draw
+drawnow;
+
+end
+
+function plotlander(fig, pos, R, T, M)
+% PLOTLANDER plots a representation of the Lunar Lander on the scene. It
+% may also plot the flames coming from the different thrusters.
+
+% Vertices of the ascent stage in body frame
+ah = 2.832;  % Ascent stage height (m)
+aw = 4.29;   % Ascent stage width (m)
+ascent_vertices_B = [-aw/2, aw/2, aw/2,         0,     -aw/2;
+                     -1,   -1,    (2/3)*ah - 1, ah - 1, (2/3)*ah - 1];
+N_ascent_vertices = size(ascent_vertices_B, 2);
+
+% Vertices of th descent stage in body frame
+dh = 3.231;  % Descent stage height (m)
+dw = 9.4;   % Ascent stage width (m)
+descent_vertices_B = [-dw/2,    0,         dw/2,    aw/2, -aw/2;
+                      -dh - 1, -dh/2 - 1, -dh - 1, -1,    -1];
+N_descent_vertices = size(descent_vertices_B, 2);
+
+% Plot thruster if needed
+if T(2) > 0
+    thrust_vertices_B = (T(2)/45000)*[-1,  0,  1, 0;  % 45000 is the normalizing/max thrust
+                                      -2, -4, -2, 0];
+	thrust_vertices_B(2,:) = thrust_vertices_B(2,:) - dh;
+	N_thrust_vertices = size(thrust_vertices_B, 2);
+end
+
+% Plot RCS thrusters if needed
+if M ~= 0
+    RCS_vertices_B = (M/45000)*[-1,  0,  1, 0;  % 45000 is the normalizing/max thrust
+                                -2, -4, -2, 0];
+    RCS_vertices_BR = RCS_vertices_B;
+    RCS_vertices_BL = -RCS_vertices_B;
+    RCS_vertices_BR(1,:) = RCS_vertices_BR(1,:) + aw/2 + 0.1;
+    RCS_vertices_BL(1,:) = RCS_vertices_BL(1,:) - aw/2 - 0.1;
+    N_RCS_vertices = size(RCS_vertices_B, 2);
+end
+
+% Rotate vertices
+ascent_vertices = zeros(size(ascent_vertices_B));
+for n = 1:N_ascent_vertices
+    ascent_vertices(:,n) = R*ascent_vertices_B(:,n);
+end
+descent_vertices = zeros(size(descent_vertices_B));
+for n = 1:N_descent_vertices
+    descent_vertices(:,n) = R*descent_vertices_B(:,n);
+end
+if T(2) > 0
+    thrust_vertices = zeros(size(thrust_vertices_B));
+    for n = 1:N_thrust_vertices
+        thrust_vertices(:,n) = R*thrust_vertices_B(:,n);
+    end
+end
+if M ~= 0
+    RCS_vertices_R = zeros(size(RCS_vertices_BR));
+    RCS_vertices_L = zeros(size(RCS_vertices_BL));
+    for n = 1:N_RCS_vertices
+        RCS_vertices_R(:,n) = R*RCS_vertices_BR(:,n);
+        RCS_vertices_L(:,n) = R*RCS_vertices_BL(:,n);
+    end
+end
+    
+
+% Translate vertices
+ascent_vertices =  ascent_vertices +  pos;
+descent_vertices = descent_vertices + pos;
+if T(2) > 0
+    thrust_vertices = thrust_vertices + pos;
+end
+if M ~= 0
+    RCS_vertices_R = RCS_vertices_R + pos;
+    RCS_vertices_L = RCS_vertices_L + pos;
+end
+
+% Plot lander
+figure(fig);
+patch(ascent_vertices(1,:),  ascent_vertices(2,:),  [.6 .6 .6]);
+patch(descent_vertices(1,:), descent_vertices(2,:), [.8 .6 .1]);
+
+% Plot thruster (if needed)
+if T(2) > 0
+    patch(thrust_vertices(1,:), thrust_vertices(2,:), [1, .8, 0]);
+end
+
+% Plot RCS thrusters (if needed)
+if M ~= 0
+    patch(RCS_vertices_R(1,:), RCS_vertices_R(2,:), [1, .8, 0]);
+    patch(RCS_vertices_L(1,:), RCS_vertices_L(2,:), [1, .8, 0]);
+end
+
+end
+
+function dispinfo(ground, pos, R, vel, w, contact_point_B, m_dry, m_fuel, m_RCS)
+% DISPINFO displays on the command window information about the lunar
+% lander. It displays: position, velocity, distance to the ground, total
+% mass of the lander, and fuel left.
+
+% Compute angle
+theta = sign(R(2,2))*(180/pi)*acos(trace(R)/2);
+
+% Compute distance to the ground
+contact_point = R*contact_point_B + pos;
+ground_dist = contact_point(2) - ground(contact_point(1));
+
+% Clear command window;
+clc;
+
+% Display information
+fprintf('               x          y\n');
+fprintf('Position: %6.2f m   %6.2f m\n', pos(1), pos(2));
+fprintf('Velocity: %6.2f m/s %6.2f m/s\n', vel(1), vel(2));
+fprintf('Angle to horizon:   %6.2f degrees\n', theta);
+fprintf('Angular velocity:   %6.2f rad/s\n', w);
+fprintf('Ground dist:        %6.2f m\n', ground_dist);
+fprintf('Thruster fuel left: %6.2f kg\n', m_fuel);
+fprintf('RCS fuel left:      %6.2f kg\n', m_RCS);
+fprintf('Total mass:         %6.2f kg\n', m_dry + m_fuel + m_RCS);
+
+end
+
+
+
+function keypresscallback(fig, evt)
+
+setappdata(0, 'key', evt.Key);
+
+end
+
+function keyreleasecallback(fig, evt)
+
+setappdata(0, 'key', []);
+
+end
